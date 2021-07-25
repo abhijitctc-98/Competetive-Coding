a207334c257de776479215517ccb1c0809197aad
abhijitctc-98 <85300599+abhijitctc-98@users.noreply.github.com>
2021-07-25 11:52:16 +0530

    Create Single Number II_LeetCode.java

---
+++ Single Number II_LeetCode.java
@@ -0,0 +1,45 @@
+/*
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each element in nums appears exactly three times except for one element which appears once.
+
+*/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        if((nums == null) || (nums.length == 0)) {
+            return 0;
+        }
+        
+        Map<Integer , Integer> map = new HashMap<Integer , Integer>();
+        
+        for(int i : nums) {
+            map.put(i , map.getOrDefault(i , 0) + 1);
+        }
+        
+        for(int key : map.keySet()) {
+            if(map.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
