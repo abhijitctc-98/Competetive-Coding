908b541e706ec91c7155ca639dc17029f9e799e3
abhijitctc-98 <85300599+abhijitctc-98@users.noreply.github.com>
2021-07-28 10:52:25 +0530

    Create Binary Tree Preorder Traversal_LeetCode.java

---
+++ Binary Tree Preorder Traversal_LeetCode.java
@@ -0,0 +1,66 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return dfs(root , new ArrayList());
+    }
+    private List<Integer> dfs(TreeNode root , List<Integer> res) {
+        if(root == null) {
+            return res;
+        }
+        res.add(root.val);
+        res = dfs(root.left , res);
+        res = dfs(root.right , res);
+        
+        return res;
+    }
+}
